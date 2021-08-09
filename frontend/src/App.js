77fc7d8bda41c885a2d3bb711cff8b7ca294aecc
Light_Bearing <hilo@list.ru>
2021-08-10 00:48:13 +0300

    Begin frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,43 @@
+import React, {useState,useEffect} from 'react';
+import './App.css';
+
+function App() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        fetch("/riders")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setItems(result);
+                },
+                // Примечание: важно обрабатывать ошибки именно здесь, а не в блоке catch(),
+                // чтобы не перехватывать исключения из ошибок в самих компонентах.
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [])
+
+    if (error) {
+        return <div>Ошибка: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Загрузка...</div>;
+    } else {
+        return (
+            <ul>
+                {items.map(item => (
+                    <li key={item.id}>
+                        {item.id} {item.surname} {item.name}
+                    </li>
+                ))}
+            </ul>
+        );
+    }
+}
+
+export default App;
