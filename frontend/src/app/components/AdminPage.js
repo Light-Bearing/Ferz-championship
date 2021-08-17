542ca788ee2c1e9a8bea4e0eb62169cef1649e6c
Light_Bearing <hilo@list.ru>
2021-08-17 23:31:21 +0300

    Накидал меню администратора

--- frontend/src/app/components/AdminPage.js
+++ frontend/src/app/components/AdminPage.js
@@ -1,55 +1,95 @@
 import AppNavbar from './AppNavbar';
 import React, {Component} from 'react';
-import {Alert, Container} from 'reactstrap';
+import {Alert, Container, FormGroup, Label} from 'reactstrap';
 import BackendService from '../services/BackendService';
+import {Form, Tab, Tabs} from "react-bootstrap";
 
 class AdminPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state={
-      content: "",
-      error: ""
+    constructor(props) {
+        super(props);
+        this.state = {
+            mainJadge: [],
+            error: ""
+        }
     }
-  }
 
-  componentDidMount() {
-    BackendService.getAdminBoard()
-      .then( response => {
-        this.setState({
-          content: response.data
-        })
-      } , error => {
-        console.log(error);
-        this.setState({
-          error: error.toString()
-        }); 
-      });    
-  }
+    componentDidMount() {
+        BackendService.getAdminBoard()
+            .then(response => {
+                this.setState({
+                    content: response.data
+                })
+            }, error => {
+                console.log(error);
+                this.setState({
+                    error: error.toString()
+                });
+            });
+    }
 
-  render() {
-    return (
-      <div>
-        <AppNavbar/>
-        <Container fluid>
-          {
-            this.state.content ? (
-              <div style={{marginTop: "20px"}}>
-                <Alert variant="info">
-                  <h2>{this.state.content}</h2>
-                </Alert>
-              </div>
-            ) : (
-              <div style={{marginTop: "20px"}}>
-                <Alert variant="danger">
-                  {this.state.error}
-                </Alert>
-              </div>
-            )
-          }
-        </Container>
-      </div>
-    );
-  }
+    handleChangeSelect = (event) => {
+        console.log(event.target.value);
+    }
+
+    render() {
+        return (
+            <div>
+                <AppNavbar/>
+                <Container fluid>
+                    {
+                        this.state.content ? (
+                            <div style={{marginTop: "20px"}}>
+                                <Alert variant="info">
+                                    <Tabs defaultActiveKey="championship" id="uncontrolled-tab-example"
+                                          className="mb-3">
+                                        <Tab eventKey="championship" title="Championship">
+                                            <Form onSubmit={this.props.action === 'A' ? this.signUp : this.update}>
+                                                <FormGroup>
+                                                    <Label>Main judge</Label>
+                                                    <Form.Control
+                                                        as="select"
+                                                        custom
+                                                        onChange={this.handleChangeSelect.bind(this)}
+                                                    >
+                                                        {this.state.mainJadge.map(
+                                                            mainJadge => <option
+                                                                value={mainJadge.id}
+                                                                key={mainJadge.id}
+                                                            >{mainJadge.surname + " " + mainJadge.name + (mainJadge.patronymic ? " " + mainJadge.patronymic : "")}</option>)}
+                                                    </Form.Control>
+                                                </FormGroup>
+                                                <FormGroup>
+                                                    <Label>Judges</Label>
+                                                </FormGroup>
+                                                <FormGroup>
+                                                    <Label>Assessments</Label>
+                                                </FormGroup>
+                                                <FormGroup>
+                                                    <Label>Ramps</Label>
+                                                </FormGroup>
+                                            </Form>
+                                        </Tab>
+                                        <Tab eventKey="categoryAssessment" title="Assessment">
+
+                                        </Tab>
+                                        <Tab eventKey="ramps" title="Ramps">
+
+                                        </Tab>
+                                    </Tabs>
+                                </Alert>
+                            </div>
+                        ) : (
+                            <div style={{marginTop: "20px"}}>
+                                <Alert variant="danger">
+                                    {this.state.error}
+                                </Alert>
+                            </div>
+                        )
+                    }
+                </Container>
+            </div>
+        );
+    }
 }
 
 export default AdminPage;
