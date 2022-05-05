b6cf0434c16951cbd389635c7b5b5fff314227c5
Alexey Belov <vaelastras@gmail.com>
2022-05-05 14:03:21 +0300

    added stars

--- frontend/src/components/AppNavBar/AppNavbar.js
+++ frontend/src/components/AppNavBar/AppNavbar.js
@@ -1,29 +1,23 @@
-import React, {Component} from 'react';
-import {Collapse, Nav, Navbar, NavbarText, NavbarToggler, NavItem} from 'reactstrap';
+import React, { useEffect, useState } from 'react';
 import AuthenticationService from '../../assets/services/AuthenticationService';
 import {Link, NavLink} from "react-router-dom";
 import './AppNavBar.css';
+import {useNavigate} from "react-router";
 
 
-class AppNavbar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {isOpen: false};
-        this.toggle = this.toggle.bind(this);
-
-        this.state = {
-            roles: [],
-            showUser: false,
-            showPM: false,
-            showAdmin: false,
-            username: undefined,
-            surname: undefined,
-            name: undefined,
-            login: false
-        };
-    }
-
-    componentDidMount() {
+function AppNavbar() {
+    const navigate = useNavigate();
+    const [state, setState] = useState({
+        roles: [],
+        showUser: false,
+        showPM: false,
+        showAdmin: false,
+        username: undefined,
+        surname: undefined,
+        name: undefined,
+        login: false
+    });
+    useEffect(() => {
         const user = AuthenticationService.getCurrentUser();
 
         if (user) {
@@ -33,7 +27,7 @@ class AppNavbar extends Component {
                 roles.push(authority.authority)
             });
 
-            this.setState({
+            setState({
                 roles,
                 showAll: roles.includes("ROLE_MAIN_JUDGE") || roles.includes("ROLE_JUDGE") || roles.includes("ROLE_PM") || roles.includes("ROLE_ADMIN"),
                 showJudge: roles.includes("ROLE_JUDGE") || roles.includes("ROLE_ADMIN"),
@@ -46,71 +40,47 @@ class AppNavbar extends Component {
                 name: user.name
             });
         }
-    }
+    },[]);
 
-    signOut = () => {
+    const signOut = () => {
         AuthenticationService.signOut();
-        this.props.history.push('/', { replace: true });
+        navigate('/', { replace: true });
         window.location.reload();
     }
 
-    toggle() {
-        this.setState({
-            isOpen: !this.state.isOpen
-        });
+    const toggle = () => {
+        setState({isOpen: !state.isOpen});
     }
 
-
-
-    render() {
-        return (
-            <nav className='container-fluid d-flex justify-content-between navbar navbar-dark bg-dark navbar-expand' >
-                <div id={1} className='d-flex align-items-center gap-3'>
-                    {this.state.showMainJudge && <NavLink className={({ isActive }) => (isActive ? 'active link' : 'inactive link')} to="/main_judge">Main judge</NavLink>}
-                    {this.state.showJudge && <NavLink className={({ isActive }) => (isActive ? 'active link' : 'inactive link')} to="/judge">Judge</NavLink>}
-                    {this.state.showAll && <NavLink className={({ isActive }) => (isActive ? 'active link' : 'inactive link')} to="/riders">Rider List</NavLink>}
-                    {this.state.showPM && <NavLink className={({ isActive }) => (isActive ? 'active link' : 'inactive link')} to="/pm">PM</NavLink>}
-                    {this.state.showAdmin && <NavLink className={({ isActive }) => (isActive ? 'active link' : 'inactive link')} to="/admin">Admin</NavLink>}
-                    {this.state.showAdmin && <NavLink className={({ isActive }) => (isActive ? 'active link' : 'inactive link')} to="/users">User list</NavLink>}
-                </div>
-                <div id={2}>
-                    {this.state.login && (
-                        <div className='navbar d-flex justify-content-center '>
-                            <p className='text-light m-0 text'>
-                                Signed in as: <Link to="/profile">{this.state.surname + " " + this.state.name}</Link>
-                            </p>
-                        </div>
-                    )}
-                </div>
-                <div id={3}>
-                    <Collapse isOpen={this.state.isOpen} navbar>
-                        {
-                            this.state.login ? (
-                                <button className='btn btn-md btn-danger' onClick={this.signOut}>SignOut</button>
-                            ) : (
-                                <Nav className="ml-auto" navbar>
-                                    <button className='btn btn-lg btn-danger'>
-                                        <NavLink to="/signin">Signin</NavLink>
-                                    </button>
-                                </Nav>
-                            )
-                        }
-                    </Collapse>
-                </div>
-
-
-
-
-                <NavbarToggler onClick={this.toggle}/>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-
-            </nav>
-        );
-    }
+    return (
+        <nav className='container-fluid d-flex justify-content-between navbar navbar-dark bg-dark navbar-expand sticky' >
+            <div className='d-flex align-items-center gap-3 fs-6'>
+                { state.showMainJudge && <NavLink className={({ isActive }) => (isActive ? 'active' : 'inactive')} to="/main_judge">Main judge</NavLink> }
+                { state.showJudge && <NavLink className={({ isActive }) => (isActive ? 'active' : 'inactive')} to="/judge">Judge</NavLink> }
+                { state.showAll && <NavLink className={({ isActive }) => (isActive ? 'active' : 'inactive')} to="/riders">Rider List</NavLink> }
+                { state.showPM && <NavLink className={({ isActive }) => (isActive ? 'active' : 'inactive')} to="/pm">PM</NavLink> }
+                { state.showAdmin && <NavLink className={({ isActive }) => (isActive ? 'active' : 'inactive')} to="/admin">Admin</NavLink> }
+                { state.showAdmin && <NavLink className={({ isActive }) => (isActive ? 'active' : 'inactive')} to="/users">User list</NavLink> }
+            </div>
+            <div>
+                {state.login && (
+                    <div className='navbar d-flex justify-content-center '>
+                        <p className='text-light m-0 text text-center'>
+                            Signed in as: <Link to="/profile">{state.surname + " " + state.name}</Link>
+                        </p>
+                    </div>
+                )}
+            </div>
+            <div>
+                { state.login
+                    ? <button className='btn btn-md btn-danger' onClick={()=> signOut()}>SignOut</button>
+                    : <button className='btn btn-lg btn-danger'>
+                        <NavLink to="/signin">Signin</NavLink>
+                       </button>
+                }
+            </div>
+        </nav>
+    );
 }
 
 export default AppNavbar;
